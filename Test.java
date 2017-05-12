abd2e32b166ddaf4468499c3494a4a57a828507a
salmahn <salmahnans@gmail.com>
2017-05-12 19:43:51 +0300

    Add files via upload
    Code for the test

---
+++ Test.java
@@ -0,0 +1,25 @@
+package com.android.practice;
+
+/**
+ * Created by salmah on 5/12/17.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Welcome to code2flow");
+        boolean inDoubt = true;
+        boolean ready = true;
+        boolean ready1 = false;
+        if (inDoubt) {
+            System.out.println("Press help");
+            while (ready == false) {
+                System.out.println("Read help");
+            }
+            if (ready1 == false) {
+                System.out.println("My name is Chris");
+
+            }
+        }
+        System.out.println("Improve your workflow");
+    }
+}
+
